fe9270cf00997655ef07f8d68ded1e76953f0008
Cheng Dong <dong0085@algonquinlive.com>
2024-10-05 19:09:23 -0400

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,34 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  if (
+    Number.isInteger(num1) &&
+    Number.isInteger(num2) &&
+    num1 > -1 &&
+    num2 > -1 &&
+    num1 < num2
+  ) {
+    let sum = 0;
+    for (let i = num1, target = num2; i <= target; i++) {
+      sum += i;
+    }
+    return sum;
+  } else if (
+    Number.isInteger(num1) &&
+    Number.isInteger(num2) &&
+    num1 > -1 &&
+    num2 > -1 &&
+    num1 > num2
+  ) {
+    let sum = 0;
+    for (let i = num2, target = num1; i <= target; i++) {
+      sum += i;
+    }
+    return sum;
+  } else {
+    return "ERROR";
+  }
 };
+
+console.log(sumAll(1, 5));
 
 // Do not edit below this line
 module.exports = sumAll;
