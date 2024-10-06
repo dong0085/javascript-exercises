37707d49d3faeed5290f483de12971e59b5b4ae8
Cheng Dong <dong0085@algonquinlive.com>
2024-10-05 20:19:27 -0400

    Complete exercise 6-7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (x) {
+  let y = ((x - 32) * 5) / 9;
+  return Math.round(y * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (x) {
+  let y = x * (9 / 5) + 32;
+  return Math.round(y * 10) / 10;
 };
+
+console.log(convertToCelsius(80));
+console.log(convertToFahrenheit(30));
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
